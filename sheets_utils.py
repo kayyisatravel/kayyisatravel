d256590b440638d2389d9254df2adcc7814a0472
kayyisatravel <rizalaqsa@gmail.com>
2025-06-08 16:57:33 +0700

    Add files via upload

--- sheets_utils.py
+++ sheets_utils.py
@@ -1,17 +1,19 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+import streamlit as st
 
 def connect_to_gsheet(SHEET_ID, worksheet_name="Data"):
     scope = [
         "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/drive"
     ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("tiketapp-462002-ec583e9f9b95.json", scope)
-    client = gspread.authorize(creds)
 
+    creds_dict = st.secrets["gcp_service_account"]  # Ambil dari secrets.toml
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(creds)
     sheet = client.open_by_key(SHEET_ID)
-    worksheet = sheet.worksheet("Data")
+    worksheet = sheet.worksheet(Data)
     return worksheet
 
 def append_dataframe_to_sheet(df, worksheet):

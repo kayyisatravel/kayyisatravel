70f5a48a69312ac60e59890806097ef9ba6928f5
kayyisatravel <rizalaqsa@gmail.com>
2025-06-11 19:10:18 +0000

    Update process_ocr.py

--- process_ocr.py
+++ process_ocr.py
@@ -341,7 +341,8 @@ def process_ocr_text_multiple(text):
 
     kode_booking = extract_booking_code(cleaned)
     hotel = clean_hotel_name(extract_hotel_name(cleaned_lines))
-    durasi = extract_duration_days_hotel(cleaned)
+    checkin, checkout = extract_dates_hotel(cleaned)
+    durasi = extract_duration_days_hotel(checkin, checkout)
     jumlah_kamar = extract_room_count(cleaned)
     harga_beli_total, harga_jual_total = extract_price_info(cleaned)
     customer_names = extract_customer_names(cleaned_lines)

c048e9f977317b1a9c4cea517e10f6ecc0528822
kayyisatravel <rizalaqsa@gmail.com>
2025-07-03 23:10:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1294,7 +1294,7 @@ with st.expander("📘 Laporan Keuangan Lengkap"):
         total_beli = df_filtered["Harga Beli (Num)"].sum()
         total_profit = total_jual - total_beli
 
-        col1, col2, col3 = st.columns(3)
+        #col1, col2, col3 = st.columns(3)
         col1.metric("💰 Total Penjualan", f"Rp {int(total_jual):,}".replace(",", "."))
         col2.metric("💸 Total Pembelian", f"Rp {int(total_beli):,}".replace(",", "."))
         col3.metric("📈 Profit", f"Rp {int(total_profit):,}".replace(",", "."))

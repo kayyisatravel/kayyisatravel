8a17be190fb4a5da8164657f44aeb1513f254420
kayyisatravel <rizalaqsa@gmail.com>
2025-07-03 23:20:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1294,10 +1294,11 @@ with st.expander("📘 Laporan Keuangan Lengkap"):
         total_beli = df_filtered["Harga Beli (Num)"].sum()
         total_profit = total_jual - total_beli
 
-        col1, col2, col3 = st.columns([1, 2, 1])
+        col1, col2, col3 = st.columns([1, 1])
         col1.metric("💰 Total Penjualan", f"Rp {int(total_jual):,}".replace(",", "."))
         col2.metric("💸 Total Pembelian", f"Rp {int(total_beli):,}".replace(",", "."))
-        st.metric("📈 Profit", f"Rp {int(total_profit):,}".replace(",", "."))
+        with col2:
+            st.metric("📈 Profit", f"Rp {int(total_profit):,}".replace(",", "."))
             
         # Grafik Tren Penjualan
         st.markdown("### 📈 Grafik Tren Penjualan")

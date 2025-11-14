de64656d175cc132ad73a75571aa72a54ce6f087
kayyisatravel <rizalaqsa@gmail.com>
2025-11-14 12:06:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ def buat_invoice_pdf(data, tanggal_invoice, unique_invoice_no, output_pdf_filena
     pdf.cell(80, 6, "-", ln=True)
     pdf.set_font("Arial", "", 6)
 
-    num_rows = len(bank_list)  # total baris daftar bank
+    #num_rows = len(bank_list)  # total baris daftar bank
 
     # --- KANAN (TEMPAT/TANGGAL + TTD) ---
     pdf.set_xy(right_x + 45, pdf.get_y())  # sesuaikan dengan tinggi bank list

6fade2c1165c99b9ba468de88cde2e9d4df82627
kayyisatravel <rizalaqsa@gmail.com>
2025-11-17 18:20:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2268,7 +2268,7 @@ def parse_cashflow_from_data(df_data, df_cashflow_existing):
 # Input Manual Cashflow
 # ---------------------------
 with st.expander("✏️ Input Data Cashflow Manual"):
-    ws_cashflow = client.open("1idBV7qmL7KzEMUZB6Fl31ZeH5h7iurhy3QeO4aWYON8").worksheet("Arus Kas")
+    ws_cashflow = client.open(SHEET_ID).worksheet("Arus Kas")
     tanggal = st.date_input("Tanggal", value=date.today(), key="tgl_input")
     tipe = st.selectbox("Tipe", ["Masuk", "Keluar"], key="tipe_input")
 

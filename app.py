91a40b5dcf3f6a7aab234e91880b1db26712aebf
kayyisatravel <rizalaqsa@gmail.com>
2026-03-13 11:31:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ names = ["User A", "User B"]
 usernames = ["usera", "userb"]
 passwords = ["12345", "67890"]  # Untuk production sebaiknya pakai hash
 
-hashed_passwords = stauth.Hasher(passwords).generate()
+hashed_passwords = stauth.Hasher().generate(passwords)
 
 authenticator = stauth.Authenticate(
     names,

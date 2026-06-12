315075e705b283e4ce0116c9cee628d7e13d6952
kayyisatravel <rizalaqsa@gmail.com>
2026-06-12 20:01:29 +0700

    Update generatornew.py

--- generatornew.py
+++ generatornew.py
@@ -7,9 +7,9 @@ from google.genai import types
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
-# =====================================================================
-# 1. BLUEPRINT PYDANTIC SCHEMA (DISELARASKAN DENGAN KAMUS DATA LAMA ANDA)
-# =====================================================================
+# =============================
+# 1. BLUEPRINT PYDANTIC SCHEMA 
+# =============================
 
 class PenumpangKeretaSchema(BaseModel):
     nama: str = Field(description="""
